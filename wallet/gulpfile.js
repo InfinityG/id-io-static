717773626113115c0762c05f5e4ab7e24829caa7
Grant Pidwell <grantpidwell@infinity-g.com>
2015-06-26 15:36:02 +0100

    Modified gulpfile

--- wallet/gulpfile.js
+++ wallet/gulpfile.js
@@ -179,7 +179,7 @@ gulp.task('html', function() {
 gulp.task('less', function () {
   gulp.src(['less/app.less', 'less/responsive.less'])
     .pipe(less({
-      paths: [ path.resolve(__dirname, 'less'), path.resolve(__dirname, 'bower_components') ]
+      paths: [ 'less', 'bower_components' ]
     }))
     .pipe(mobilizer('app.css', {
       'app.css': {
